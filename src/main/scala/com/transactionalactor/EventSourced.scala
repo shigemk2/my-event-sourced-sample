feba8c99c63c547faddc120a38a395af1dedfdde
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-31 03:46:11 +0900

    Add classes

--- src/main/scala/com/transactionalactor/EventSourced.scala
+++ src/main/scala/com/transactionalactor/EventSourced.scala
@@ -4,3 +4,14 @@ import akka.actor._
 
 object EventSourcedDriver extends CompletableApp(4) {
 }
+
+trait DomainEvent
+
+case class StartOrder(orderId: String, customerInfo: String)
+case class OrderStarted(orderId: String, customerInfo: String) extends DomainEvent
+
+case class AddOrderLineItem(productId: String, units: Int, price: Double)
+case class OrderLineItemAdded(productId: String, units: Int, price: Double) extends DomainEvent
+
+case class PlaceOrder()
+case class OrderPlaced() extends DomainEvent
