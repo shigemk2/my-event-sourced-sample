90769dbe7c40356ee9a87fb121d687b57a3a13e5
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-31 03:46:52 +0900

    Add Order

--- src/main/scala/com/transactionalactor/EventSourced.scala
+++ src/main/scala/com/transactionalactor/EventSourced.scala
@@ -1,5 +1,6 @@
 package com.transactionalactor
 
+import akka.actor.Actor.Receive
 import akka.actor._
 
 object EventSourcedDriver extends CompletableApp(4) {
@@ -15,3 +16,26 @@ case class OrderLineItemAdded(productId: String, units: Int, price: Double) exte
 
 case class PlaceOrder()
 case class OrderPlaced() extends DomainEvent
+
+class Order extends EventsourcedProcessor {
+  var state: OrderState = _ // new OrderState()
+
+  def updateState(event: DomainEvent): Unit =
+    state = state.update(event)
+
+  val receiveRecover: Receive = {
+    case event: DomainEvent =>
+      updateState(event)
+    case SnapshotOffer(_, snapshot: OrderState) =>
+      state = snapshot
+  }
+
+  val receiveCommand: Receive = {
+    case startOrder: StartOrder =>
+      persist(OrderStarted(startOrder.orderId, startOrder.customerInfo)) (updateState)
+    case addOrderLineItem @ AddOrderLineItem =>
+    //persist(OrderStarted(addOrderLineItem., startOrder.customerInfo)) (updateState)
+    case placeOrder @ PlaceOrder =>
+  }
+}
+
